b3540a31a9035d48786f2c9048fa7e866b1d7cae
Rock3f <tanguy.badier@ynov.com>
2018-01-26 15:02:05 +0100

    Exercise 2

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ let chalk = require('chalk');
 const app = require('./src/app');
 
 // port
-const port = 3000;
+//Les deux premiers éléments de l'index argv sont utilisés par NODE.
+const port = process.argv[2];
 
 // start server here
 app.listen(port)
+console.log('Listenning on the port ' + port)
 console.log(chalk.green('Hello web server'));
