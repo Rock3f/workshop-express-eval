85855ae546eb8e5600c25399d4944d0e569ad573
Rock3f <tanguy.badier@ynov.com>
2018-01-26 16:07:17 +0100

    Exercise 5

--- src/app.js
+++ src/app.js
@@ -12,10 +12,37 @@ app.get('/bingo', function(req, res) {
         if(err) {
             return console.log(err);   
         }
+        var message;
         var listNumbers = content.toString().trim().split('\r').join(',');
-        console.log(listNumbers);
-    
+        
+        console.log();
 
-        res.send(listNumbers);
+        if(req.query["mynumbers"] == undefined)
+        {
+            message = 'The bingo game is already started and known numbers are ' +   listNumbers;
+        }
+        else 
+        {
+            var myNumbersArray = req.query["mynumbers"].split(',');
+            var listNumbersLength = listNumbers.split(',').length;
+            var goodAnswer = 0;
+
+            myNumbersArray.forEach(element => {
+                if(listNumbers.indexOf(element) != -1) {
+                    goodAnswer++;
+                }
+            });
+
+            if(goodAnswer != listNumbersLength) {
+                message = 'The bingo game is already started, sorry your numbers doesn\'t match with known numbers 31, 10, 80, 44, 66 ; so you can not say Bingo'
+            }
+            else
+            {
+                message =  'Bingo';
+            }
+
+        }
+
+        res.send(message);
     });
 });
