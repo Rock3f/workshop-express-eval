d3ad57eb9cc3f50291b3d6c13313efc12b4888aa
Rock3f <tanguy.badier@ynov.com>
2018-01-26 15:31:33 +0100

    Exercise 4

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var fs = require('fs');
 
 const app = express();
 
 module.exports = app;
 
 app.get('/bingo', function(req, res) {
-  res.end('Bingo');
-})
+  //res.send('Bingo');
+
+    var file = fs.readFile("./src/numbers.txt","utf8", (err, content) => {
+        if(err) {
+            return console.log(err);   
+        }
+        var listNumbers = content.toString().trim().split('\r').join(',');
+        console.log(listNumbers);
+    
+
+        res.send(listNumbers);
+    });
+});
